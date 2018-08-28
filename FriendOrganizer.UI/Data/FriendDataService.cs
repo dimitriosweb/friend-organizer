62181d47dd117d1b2c8d78ca3fcf62be56c02e8f
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-08-28 14:44:10 +0200

    Create MainViewModel and implement INotifyPropertyChanged.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend {FirstName = "Roberto", LastName = "Ferrer"};
+            yield return new Friend { FirstName = "Bernardo", LastName = "Stefano" };
+            yield return new Friend { FirstName = "Radovan", LastName = "Slovak" };
+            yield return new Friend { FirstName = "Anca", LastName = "Roma" };
+        }
+    }
+}
